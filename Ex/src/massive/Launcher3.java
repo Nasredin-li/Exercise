9bb89a63a404dd006180e20b3c815c9dd6bc8738
Администратор <@drawing3.ROGAN.local>
2016-03-22 16:30:23 +0200

    Plus going

--- Ex/src/massive/Launcher3.java
+++ Ex/src/massive/Launcher3.java
@@ -1,33 +1,68 @@
 package massive;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Launcher3 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader inputStream = null;
 		java.io.PrintWriter outputStream = null;
 		try {
-			outputStream = new java.io.PrintWriter(new java.io.FileWriter("d:\\Serge\\java\\Maze3.htm"));
-
-			int a = 10;
-			int b = 10;
+			int a = 20;
+			int b = 20;
 			int[] gift = new int[a / 3];
 			for (int f = 0; f < gift.length; f++) {
 				gift[f] = ((int) (Math.random() * a * b));
 			}
-
 			Mass3 mass = new Mass3(a, b);
 			int[][] maze = mass.getRealMazeSorted();
+			inputStream = new BufferedReader(
+					new FileReader("d:\\Serge\\java\\Ded-Moroz.js"));
+			ArrayList<String> javascript = new ArrayList<String>();
+			String c;
+			while ((c = inputStream.readLine()) != null) {
+				if(c.startsWith("var data")==false){
+					javascript.add(c);	
+				}
+				
+			
+			}
+			outputStream = new java.io.PrintWriter(new java.io.FileWriter("d:\\Serge\\java\\Ded-Moroz.js"));
+			
+			for (int i = 0; i < a * b;i++) {
+				
+				outputStream.println("var data" + i + "=[" + maze[i][2] + ", " + maze[i][3] + ", " + maze[i][4]
+						+ ", " + maze[i][5] + "];");
+			}
+			for (String e : javascript) {
+				outputStream.println(e);
+			}
+			
+			if (outputStream != null) {
+				outputStream.close();
+			}
+			
+
+			outputStream = new java.io.PrintWriter(new java.io.FileWriter("d:\\Serge\\java\\Maze3.htm"));
+
 			outputStream.println("<!DOCTYPE HTML>");
 			outputStream.println("<html><head>" + "<meta charset=\"UTF-8\">" + "<title>Maze3</title>" + "</head>"
-					+ "<body>" + "<table style=" + "\"" + "border: 2px solid rgb(221, 221, 221); display: table;\">");
+					+ "<body>"
+					+"<h2>Labirint</h2p><p id=\"x\">"+a+"</p><p> x </p><p id=\"y\">"+b+"</p>"
+					+ "<button type=\"button\" onclick=\"left()\">Left</button>"
+					+ "<button type=\"button\" onclick=\"right()\">Right</button>"
+					+ "<button type=\"button\" onclick=\"top()\">Top</button>"
+					+ "<button type=\"button\" onclick=\"bottom()\">Bottom</button>" + "<table style=" + "\""
+					+ "border: 2px solid rgb(221, 221, 221); display: table;\">");
 			for (int i = 0; i < a * b;) {
 				for (int y = 0; y < a; y++) {
 					outputStream.println("<tr>");
 					for (int x = 0; x < b; x++) {
-						outputStream
-								.print("<td style=\"border: 2px solid rgb(221, 221, 221); width: 30px;height: 30px;");
+						outputStream.print("<td id=\"" + i + "\""
+								+ " style=\"border: 2px solid rgb(221, 221, 221); width: 30px;height: 30px;");
 
 						if (maze[i][2] != 0) {
 							outputStream.print("border-top: none;");
@@ -44,7 +79,7 @@ public class Launcher3 {
 						outputStream.println("\">");
 						for (int u = 0; u < a / 3; u++) {
 							if (i == gift[u]) {
-								outputStream.println("<IMG src=\"./new-year-composition9.gif\"");
+								outputStream.println("<IMG src=\"./new-year-composition9.gif\">");
 								break;
 							}
 						}
@@ -55,12 +90,16 @@ public class Launcher3 {
 					outputStream.println("</tr>");
 				}
 			}
+			outputStream.println("<script type=\"text/javascript\" src=\"./Ded-Moroz.js\"></script>");
 			outputStream.println("</body></html>");
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			if (inputStream != null) {
+				inputStream.close();
+			}
 			if (outputStream != null) {
 				outputStream.close();
 			}
